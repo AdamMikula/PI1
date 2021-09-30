fbbdcbee9be68a1c3cab931af96e08344dc3a3f4
z21ite13 <z21ite13@z00p3-02.SPSKNM>
2021-09-30 14:24:00 +0200

    g

--- Prváhodina/main.py
+++ Prváhodina/main.py
@@ -1,10 +1,38 @@
-X = 10
-Y = 20
+# +-,*,%,**,/ - aritmeticke operátory
+# ==,!=,<,<=, - logické operátory
 
-tmp = X
-X = Y
-Y = tmp
+#vek = int(input("Zadaj svoj vek "))
+
+#if vek >= 18:
+   #print("Vstup dalej mas dost rokov")
+   #print("Vitaj medzi nami")
+#else:
+    #print("Vstup zakazany si moc mlady!")
+#print("tento kod sa vykona vždy")
+#a = int(input("zadaj a "))
+#b = int(input("zadaj b "))
+#print(a/b)
+#if b == 0:
+  #rint("chyba! 0 sa delit neda")
+#else:
+    #print("delenie", a/b)
+day = 5
+
+#f day == 1:
+   #print("pondelok")
+#f day == 2:
+  #print("utorok")
+#f day == 3:
+   #print("streda")
+#   print("stvrtok")
+#f day == 5:
+   #print("piatok")
 
 
+stastny = input("si stastny? ")
+bohaty = input("si bohaty ")
 
+if stastny == "ano":
+    if bohaty == "ano":
+   
 
