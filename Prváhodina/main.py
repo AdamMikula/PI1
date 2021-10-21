9896781bf30230a63123af985f4909a73a191999
z21ite13 <z21ite13@z00p3-02.SPSKNM>
2021-10-21 14:18:07 +0200

    pocet pismen/znakov

--- Prváhodina/main.py
+++ Prváhodina/main.py
@@ -1,24 +1,3 @@
-# +-,*,%,**,/ - aritmeticke operátory
-# ==,!=,<,<=, - logické operátory
-
-#vek = int(input("Zadaj svoj vek "))
-
-#if vek >= 18:
-   #print("Vstup dalej mas dost rokov")
-   #print("Vitaj medzi nami")
-#else:
-    #print("Vstup zakazany si moc mlady!")
-#print("tento kod sa vykona vždy")
-#a = int(input("zadaj a "))
-#b = int(input("zadaj b "))
-#print(a/b)
-#if b == 0:
-  #rint("chyba! 0 sa delit neda")
-#else:
-    #print("delenie", a/b)
-#day = 5
-
-
 
 """
 Vek = int(input("Zadaj svoj vek"))
@@ -46,3 +25,102 @@ print("výsledok delenia sa rovná: ", X/Y)
 print("výsledok súčtu sa rovná: ", X+Y)
 print("výsledok rozdielu sa rovná: ", X-Y)
 """
+
+"""
+start = 100
+koniec = 0
+while start >= koniec:
+    print(start)
+    start = start - 5
+"""
+"""
+start = 10
+koniec = 0
+while start >= koniec:
+    if start == 5:
+        break
+    print(start)
+    start -= 1
+"""
+"""
+x = int(input("zadajte zaciatocne cislo :"))
+y = int(input("zadajte koncove cislo :"))
+while(x < y):
+    if(x % 3 == 0):
+        print(x, "je nasobok 3ky")
+     x += 1
+"""
+"""
+for premenny in range(10):
+    print(premenny)
+   
+"""
+"""
+samohlasky = "aáeéiíoóuúyý"
+slovo = input("zadaj slovo: ")
+obsahuje_sam = False
+for znak in slovo:
+    if znak in samohlasky:
+        obsahuje_sam = True
+        break
+
+if obsahuje_sam == True:
+    print("slovo obsahuje samohlasky")
+else:
+    print("slovo neobsahuje samohlasky")
+while 
+"""
+"""
+znaky = "?!%+-*/><._-,:()§ä=ˇ°"
+samohlasky = "aáeéiíoóuúyý"
+spoluhlásky = "dtnlchgkďťňjľcčžšdzdžmbpvzsfr"
+cisla = "12345678910"
+slovo = input("zadaj slovo: ")
+pocet_samohlasok = 0
+pocet_spoluhlasok = 0
+pocet_cisel = 0
+pocet_znakov = 0
+for znak in slovo:
+    if znak in samohlasky:
+        pocet_samohlasok += 1
+    if znak in spoluhlásky:
+        pocet_spoluhlasok += 1
+    if znak in cisla:
+        pocet_cisel +=1
+    if znak in znaky:
+        pocet_znakov +=1
+if pocet_samohlasok > 0:
+    print("slovo obsahuje samohlasky", pocet_samohlasok)
+else:
+    print("slovo neobsahuje samohlasky")
+if pocet_spoluhlasok > 0:
+    print("slovo obsahuje spoluhlasok", pocet_spoluhlasok)
+else:
+    print("slovo neobsahuje spoluhlásky")
+if pocet_cisel > 0:
+    print("slovo obsahuje cisel", pocet_cisel)
+else:
+    print("slovo neobsahuje cisla")
+if pocet_znakov > 0:
+    print("slovo obsahuje znakov",pocet_znakov)
+else:
+    print("slovo neobsahuje znaky")
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
