aa184ab0b2400cb892c81985f09ce15919459696
AdamMikula <91258408+AdamMikula@users.noreply.github.com>
2022-02-24 10:45:43 +0100

    Update Takmer hotové.py

--- Snake/Takmer hotové.py
+++ Snake/Takmer hotové.py
@@ -44,10 +44,14 @@ suradnice_hada = [suradnicaX, suradnicaY]
 suradnica_textX = 30
 suradnica_textY = 15
 
+dx = 0
+dy = 0
+
 suradnica_textX2 = 0
 suradnica_textY2 = 150
-SMER = "hore"
-zmena = SMER
+
+suradnica_textX3 = 190
+suradnica_textY3 = 250
 
 prehra = False
 
@@ -61,61 +65,39 @@ while not prehra:
         if event.type == pygame.QUIT:
             pygame.quit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                zmena = 'UP'
-            if event.key == pygame.K_DOWN:
-                zmena = 'DOWN'
             if event.key == pygame.K_LEFT:
-                zmena = 'LEFT'
-            if event.key == pygame.K_RIGHT:
-                zmena = 'RIGHT'
+                dx -= 10
+                dy = 0
+            elif event.key == pygame.K_RIGHT:
+                dx += 10
+                dy = 0
+            elif event.key == pygame.K_UP:
+                dy -= 10
+                dx = 0
+            elif event.key == pygame.K_DOWN:
+                dy += 10
+                dx = 0
 
-    if zmena == 'UP' and SMER != 'DOWN':
-        SMER = 'UP'
-    if zmena == 'DOWN' and SMER != 'UP':
-        SMER = 'DOWN'
-    if zmena == 'LEFT' and SMER != 'RIGHT':
-        SMER = 'LEFT'
-    if zmena == 'RIGHT' and SMER != 'LEFT':
-        SMER = 'RIGHT'
+    if suradnicaY < 390 and suradnicaY > 2 and suradnicaX < 490 and suradnicaX > 2:
+        prehra = False
+    else:
+        prehra = True
+        pygame.mixer.Sound.play(audio2)
 
-    if SMER == 'UP':
-        if suradnicaY > 2:
-            suradnicaY -= 10
-        else:
-            prehra = True
-            pygame.mixer.Sound.play(audio2)
-    if SMER == 'DOWN':
-        if suradnicaY < 390:
-            suradnicaY += 10
-        else:
-            prehra = True
-            pygame.mixer.Sound.play(audio2)
-    if SMER == 'LEFT':
-        if suradnicaX > 2:
-            suradnicaX -= 10
-        else:
-            prehra = True
-            pygame.mixer.Sound.play(audio2)
-    if SMER == 'RIGHT':
-        if suradnicaX < 490:
-            suradnicaX += 10
-        else:
-            prehra = True
-            pygame.mixer.Sound.play(audio2)
+    suradnicaX += dx
+    suradnicaY += dy
 
-    hodiny.tick(17)
-    pygame.display.update()
+    hodiny.tick(20)
 
     if suradnicaX+-5 == suradnica_jablkoX+-5 and suradnicaY+-5 == suradnica_jablkoY+-5:
         body += 1
         suradnica_jablkoX = random.randrange(1, (sirka // 10)) * 10
         suradnica_jablkoY = random.randrange(1, (vyska // 10)) * 10
         pygame.mixer.Sound.play(audio1)
-        pygame.display.update()
 
-    vypisanie_textu = typ_pismen.render(f"Score {body}", True, black)
+    vypisanie_textu = typ_pismen.render(f"Score: {body}", True, black)
     plocha.blit(vypisanie_textu, (suradnica_textX, suradnica_textY))
+
     pygame.display.update()
 
 if prehra:
@@ -129,6 +111,9 @@ while prehra:
 
     vypisanie_textu2 = typ_pismen2.render("GAME OVER ", True, cervena)
     plocha.blit(vypisanie_textu2, (suradnica_textX2, suradnica_textY2))
+
+    vypisanie_textu3 = typ_pismen.render("Tvoj počet bodov: "+str(body), True, cervena)
+    plocha.blit(vypisanie_textu3, (suradnica_textX3, suradnica_textY3))
 
     pygame.display.update()
 
